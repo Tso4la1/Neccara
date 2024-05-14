5486e65c3737495637a6c2a27041a0096947195f
Tso4la1 <tsolajohn@gmail.com>
2024-05-14 16:11:13 +0100

    ok

--- src/Components/Molecule/ProductSample/index.jsx
+++ src/Components/Molecule/ProductSample/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GenStyle } from "../../../App";
-// import { Imgs } from '../../Atoms';
+import { Text } from '../../Atoms';
+
 
 export const ProductSample = () => {
     return (
@@ -12,8 +13,16 @@ export const ProductSample = () => {
             Gap="2%"
 
         >
-            <GenStyle className='sample1'></GenStyle>
-            <GenStyle className='sample2'></GenStyle>
+            <GenStyle className='sample1'>
+                <Text className='sample1Topic' name="p" Text="New to Skincare?" />
+                <Text className='sample1Content' name="p" Text="Book A consultation with one of our Esthetication" />
+                <button className='sample1Button'><Text name="p" Text="BOOK APPOINTMENT" /></button>
+            </GenStyle>
+            <GenStyle className='sample2'>
+                <Text className='sample2Topic' name="p" Text="Not Sure Where to Start Your Skincare Journey?" />
+                <Text className='sample2Content' name="p" Text="Download our begginers skincare guide" />
+                <button className='sample2Button'><Text name="p" Text="BOOK APPOINTMENT" /></button>
+            </GenStyle>
         </GenStyle>
     )
 }
